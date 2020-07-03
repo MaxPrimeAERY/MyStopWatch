275d74b82c7ae43c65f4b59862609d95c7c7bdb9
MAX-DESKTOP <max292009@gmail.com>
2020-07-03 13:41:04 +0300

    added js for heroku

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-//Install express server
 const express = require('express');
+const http = require('http');
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/MyStopWatch'));
 
-app.get('/*', function(req,res) {
+app.use(express.static(path.join(__dirname, 'dist/my-stop-watch')));
 
-  if(req.url === '/') {
-    res.sendFile(path.join(__dirname,'/dist/MyStopWatch/index.html'));
-  }
-  else {
-    res.sendFile(path.join(__dirname, '/dist/MyStopWatch' + req.url))
-  }
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/my-stop-watch/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log("running"));
